9b13937bcca594f408a05cfe20db5cf67e089311
xuhui1 <731048736@qq.com>
2018-03-16 19:07:09 +0800

    Update train-predict.py

--- train-predict.py
+++ train-predict.py
@@ -1,1 +1,94 @@
 
+#coding=utf-8
+import pandas as pd
+from model import lgb_modelfit_nocv
+
+path = './input/'
+dtypes = {
+        'ip'            : 'uint32',
+        'app'           : 'uint16',
+        'device'        : 'uint16',
+        'os'            : 'uint16',
+        'channel'       : 'uint16',
+        'is_attributed' : 'uint8',
+        'click_id'      : 'uint32'
+        }
+
+train_df = pd.read_csv(path+"train.csv", nrows=40000000, dtype=dtypes, usecols=['ip','app','device','os', 'channel', 'click_time', 'is_attributed'])
+print('load test...')
+test_df = pd.read_csv(path+"test.csv", dtype=dtypes, usecols=['ip','app','device','os', 'channel', 'click_time', 'click_id'])
+
+
+import gc
+
+len_train = len(train_df)
+train_df=train_df.append(test_df)
+
+del test_df
+gc.collect()
+
+train_df['hour'] = pd.to_datetime(train_df.click_time).dt.hour.astype('uint8')
+train_df['day'] = pd.to_datetime(train_df.click_time).dt.day.astype('uint8')
+
+gc.collect()
+
+gp = train_df[['ip','day','hour','channel']].groupby(by=['ip','day','hour'])[['channel']].count().reset_index().rename(index=str, columns={'channel': 'qty'})
+
+train_df = train_df.merge(gp, on=['ip','day','hour'], how='left')
+
+train_df.info()
+
+test_df = train_df[len_train:]
+val_df = train_df[(len_train-3000000):len_train]
+train_df = train_df[:(len_train-3000000)]
+
+print("train size: ", len(train_df))
+print("valid size: ", len(val_df))
+print("test size : ", len(test_df))
+
+target = 'is_attributed'
+predictors = ['app','device','os', 'channel', 'hour', 'qty']
+categorical = ['app','device','os', 'channel', 'hour']
+
+sub = pd.DataFrame()
+sub['click_id'] = test_df['click_id'].astype('int')
+
+gc.collect()
+
+print("Training...")
+
+params = {
+    'learning_rate': 0.1,
+    #'is_unbalance': 'true', # replaced with scale_pos_weight argument
+    'num_leaves': 7,  # we should let it be smaller than 2^(max_depth)
+    'max_depth': 3,  # -1 means no limit
+    'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
+    'max_bin': 100,  # Number of bucketed bin for feature values
+    'subsample': .7,  # Subsample ratio of the training instance.
+    'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
+    'colsample_bytree': 0.7,  # Subsample ratio of columns when constructing each tree.
+    'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+    'scale_pos_weight':99 # because training data is extremely unbalanced
+}
+
+bst = lgb_modelfit_nocv(params,
+                        train_df,
+                        val_df,
+                        predictors,
+                        target,
+                        objective='binary',
+                        metrics='auc',
+                        early_stopping_rounds=50,
+                        verbose_eval=True,
+                        num_boost_round=300,
+                        categorical_features=categorical)
+del train_df
+del val_df
+gc.collect()
+
+print("Predicting...")
+sub['is_attributed'] = bst.predict(test_df[predictors])
+print("writing...")
+sub.to_csv('result/submit',index=False)
+print("done...")
+print(sub.info())
